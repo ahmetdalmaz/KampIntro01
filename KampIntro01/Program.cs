8c6f34c3ba589075f15d493adb9b28467577b3d8
Ahmet Dalmaz <ahmett3437@hotmail.com>
2021-01-17 17:22:30 +0300

    Add project files.

---
+++ KampIntro01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace KampIntro01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kurs kurs1 = new Kurs();
+            kurs1.KursAdi = "C";
+            kurs1.Egitmeni = "Engin Demiroğ";
+            kurs1.IzlenmeOranı = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmeni = "Ahmet Dalmaz";
+            kurs2.IzlenmeOranı = 85;
+
+            Kurs[] kurslar = new Kurs[] {kurs1, kurs2};
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi);
+            }
+        }
+    }
+
+    class Kurs 
+    {
+        public string KursAdi { get; set; }
+
+        public string Egitmeni { get; set; }
+
+        public int IzlenmeOranı { get; set; }
+
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Adi = "Çilek";
+            double fiyati = 15;
+            string Aciklama = "çilek gibi çilek";
+            string[] meyveler = new string[] { };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+            //type-safe (tip güvenli)
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("-------------");
+            }
+
+            Console.WriteLine("----------------METOTLAR------------------");
+            //instance (örnek)
+            SepetManager manager = new SepetManager();
+            manager.Ekle(urun1);
+            manager.Ekle(urun2);
+
+            manager.Ekle2("Armut", "Yeşil Armut",12,10);
+            manager.Ekle2("Elma", "Yeşil Elma", 12,9);
+            manager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12,8);
+
+        }
+    }
+}
