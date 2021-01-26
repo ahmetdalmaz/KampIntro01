63ea6df51359c297fae4950179cfa88bde64fda1
Ahmet Dalmaz <ahmett3437@hotmail.com>
2021-01-26 11:24:59 +0300

    20 Ocak

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"Ahmet","Murat","Ramiz"};
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Ahmet", "Murat", "Ramiz" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+            
+            //foreach (var item in isimler2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+        }
+    }
+}
